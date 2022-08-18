47b15ed45727f5a9f33a7ae7e076df6693b80607
aogu-takahashi <aogu_takahashi@outlook.com>
2022-08-18 15:56:19 +0900

    add デフォルトのlocalを日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Syaribatezu
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    #　デフォルトのlocaleを日本語にする
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
