54155dd99f41a2e1ba47306b9acd0cf18fffccfd
aogu-takahashi <aogu_takahashi@outlook.com>
2022-08-18 16:10:34 +0900

    add RSpecの設定

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,14 @@ module Syaribatezu
 
     # デフォルトのtimezoneを東京にする
     config.time_zone = 'Tokyo'
+
+    # RSpecの設定
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+    end
   end
 end
