16843a3a45e86ef32732ada0a4f23873ae2e5332
aogu-takahashi <aogu_takahashi@outlook.com>
2022-08-20 14:39:03 +0900

    change ユーザーモデルのアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_103151) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_043056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mountains", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_mountains", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "mountain_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mountain_id"], name: "index_user_mountains_on_mountain_id"
+    t.index ["user_id"], name: "index_user_mountains_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_103151) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "user_mountains", "mountains"
+  add_foreign_key "user_mountains", "users"
 end
