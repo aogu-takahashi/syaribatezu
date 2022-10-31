b31128033a676bf857389904c8595b84daa5d186
aogu-takahashi <aogu_takahashi@outlook.com>
2022-10-31 15:47:31 +0900

    add memo食品情報の保存

--- app/controllers/calculate_energys_controller.rb
+++ app/controllers/calculate_energys_controller.rb
@@ -100,9 +100,30 @@ class CalculateEnergysController < ApplicationController
 
   def save_memo
     if logged_in?
-      data = Memo.new(save_daata_params)
-      data.user_id = current_user.id
-      data.save
+      memo = Memo.new(memo_data_params)
+      memo.user_id = current_user.id
+      memo.save
+
+      if ration_data_params[:choco].to_i > 0
+        choco = PortableFood.new(volume: ration_data_params[:choco], ration_id: Ration.find_by(name: "チョコレート").id, memo_id: memo.id)
+        choco.save
+      end
+
+      if ration_data_params[:yokan].to_i > 0
+        yokan = PortableFood.new(volume: ration_data_params[:yokan], ration_id: Ration.find_by(name: "羊羹").id, memo_id: memo.id)
+        yokan.save
+      end
+
+      if ration_data_params[:dried_fruit].to_i > 0
+        dried_fruit = PortableFood.new(volume: ration_data_params[:dried_fruit], ration_id: Ration.find_by(name: "ドライフルーツ").id, memo_id: memo.id)
+        dried_fruit.save
+      end
+
+      if drink_data_params[:pocari].to_i > 0
+        pocari = PortableDrink.new(volume: drink_data_params[:pocari], drink_id: Drink.find_by(name: "ポカリ").id, memo_id: memo.id)
+        pocari.save
+      end
+
       redirect_to root_path
     else
       redirect_to new_user_path
@@ -123,7 +144,15 @@ class CalculateEnergysController < ApplicationController
     params.permit(:date, :temperature, :age, :sex, :height, :weight, :rucksack_weight, :course_id)
   end
 
-  def save_daata_params
-    params.permit(:temperature, :user_id, :course_id, :date)
+  def memo_data_params
+    params.permit(:temperature, :course_id, :date)
+  end
+
+  def ration_data_params
+    params.permit(:choco, :yokan, :dried_fruit)
+  end
+
+  def drink_data_params
+    params.permit(:pocari)
   end
 end
