2ef04d74c6d238bd98a33b795c299f8b3b435390
aogu-takahashi <aogu_takahashi@outlook.com>
2022-11-26 00:01:56 +0900

    add Gmailによるメール送信

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
     @user&.deliver_reset_password_instructions!
     # 「存在しないメールアドレスです」という旨の文言を表示すると、逆に存在するメールアドレスを特定されてしまうため、
     # あえて成功時のメッセージを送信させている
-    redirect_to login_path, notice: 'パスワード再設定用のメールを送信しました'
+    redirect_to login_path, success: 'パスワード再設定用のメールを送信しました'
   end
 
   def edit
@@ -25,7 +25,7 @@ class PasswordResetsController < ApplicationController
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to login_path, notice: 'パスワードを再設定しました'
+      redirect_to login_path, success: 'パスワードを再設定しました'
     else
       flash.now[:danger] = t('.fail')
       render :edit
