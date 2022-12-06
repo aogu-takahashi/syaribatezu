1c86f3ed384923091ec27716b49db42055add046
aogu-takahashi <aogu_takahashi@outlook.com>
2022-12-06 22:46:38 +0900

    fix 山一覧のview

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -2,7 +2,13 @@ class MountainsController < ApplicationController
   skip_before_action :require_login, only: [:narrowed_index]
 
   def index
-    @mountains = Mountain.all
+    @hokkaido_tohoku_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["北海道", "青森", "岩手", "宮城", "秋田", "山形", "福島"]}).order(:id)
+    @kanto_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["茨城", "栃木", "群馬", "埼玉", "千葉", "東京", "神奈川"]}).order(:id)
+    @chubu_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["新潟", "富山", "石川", "福井", "山梨", "長野", "岐阜", "静岡", "愛知"]}).order(:id)
+    @kinki_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["三重", "滋賀", "京都", "大阪", "兵庫", "奈良", "和歌山"]}).order(:id)
+    @chugoku_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["鳥取", "島根", "岡山", "広島", "山口"]}).order(:id)
+    @shikoku_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["徳島", "香川", "愛媛", "高知"]}).order(:id)
+    @kyushu_okinawa_mountains = Mountain.includes(:prefectures).where(prefectures: {name: ["福岡", "佐賀", "長崎", "熊本", "大分", "宮崎", "鹿児島", "沖縄"]}).order(:id)
   end
 
   def narrowed_index
